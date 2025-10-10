7905af3ac5597c756de47c8ee5f99850e665ec36
Durvankur-Joshi <joshidurvankur.29@gmail.com>
2025-10-10 08:27:59 +0530

    Task 4

--- src/props/Studentlist.jsx
+++ src/props/Studentlist.jsx
@@ -1,16 +1,44 @@
 import StudentCard from './StudentCard'
+import React, { useState } from 'react'
 import student from '../student.json'
 function Studentlist(props)
 {
+    const [filterdata, setfilterdata] = useState(student)
+
+   const handlefilter = (age) =>{
+     if(age>=18)
+     {
+        setfilterdata(student.filter((student)=> student.age>=18))
+     }
+     else if(age<18){
+        setfilterdata(student.filter((student) => student.age<18))
+     }
+     else{
+        setfilterdata(student)
+     }
+   }
     return(
         <>
+            <div>
+                <button onClick={()=>handlefilter()}>All</button>
+                <button onClick={()=> handlefilter(17)}>Teen</button>
+                <button onClick={()=> handlefilter(18)}>Adult</button>
+                
+
+                </div>
         {
 
-            student.map((student) =>(
+
+            // student.map((student) =>(
+            //     <div>
+            //         <StudentCard  name={student.name} cource = {student.course} age={student.age}  year={student.year} city = {student.city} email= {student.email} />
+            //     </div>
+                
+            // ))
+            filterdata.map((student) =>(
                 <div>
                     <StudentCard  name={student.name} cource = {student.course} age={student.age}  year={student.year} city = {student.city} email= {student.email} />
                 </div>
-                
             ))
         }
         </>
