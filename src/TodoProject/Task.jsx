9e80572ff122a5d6950f8818903adc23b22ee71b
Durvankur-Joshi <joshidurvankur.29@gmail.com>
2025-10-16 11:11:23 +0530

    Task 8

--- src/TodoProject/Task.jsx
+++ src/TodoProject/Task.jsx
@@ -30,26 +30,31 @@ function Task() {
 
     return (
         <>
-            <div className="app">
-                <h1>Task Manager</h1>
+            <div className="task-container  border-2 h-50 w-100 m-auto mt-5 p-3 rounded-2xl">
+                <h1 className=" flex justify-center text-2xl font-bold text-red-500">Task Manager</h1>
 
-                {/* Input Section */}
-                <div className="input-section">
+                <div className=" flex justify-center ">
                     <input
+                        className="input-section w-90 border-2 rounded-2xl p-3 my-3 "
                         type="text"
                         value={taskText}
                         onChange={handleChange}
                         placeholder="Enter a new task..."
                     />
-                    <button onClick={addTask}>Add Task</button>
+                </div>
+                <div className="flex justify-center text-xl ">
+                    <button className="border-2 w-40 h-12 mt-4 bg-green-700 rounded-3xl hover:bg-green-900 text-white " onClick={addTask}>Add Task</button>
                 </div>
 
-                {/* Conditional Rendering */}
+
+            </div>
+            <div className="w-96  max-w-md mt-10 flex justify-center m-auto">
                 {tasks.length === 0 ? (
-                    <p className="no-task">No tasks yet</p>
+                    <p className="no-task text-center text-gray-500 border-2 border-dashed border-gray-300 rounded-2xl p-5">
+                        No tasks yet
+                    </p>
                 ) : (
-                    <div className="task-list">
-                        {/* Render list of tasks */}
+                    <div className="task-list flex flex-col gap-3 border-2 border-indigo-300 rounded-2xl p-5 bg-white shadow-md">
                         {tasks.map((task, index) => (
                             <TaskCard
                                 key={index}
--- src/TodoProject/TaskCard.jsx
+++ src/TodoProject/TaskCard.jsx
@@ -6,7 +6,7 @@ function TaskCard(props) {
         <>
             <div className="task-card">
                 <p>{title}</p>
-                <button className="delete-btn" onClick={onDelete}>
+                <button className="delete-btn bg-red-600 hover:bg-red-900 text-white w-20 rounded-2xl h-10 mt-5" onClick={onDelete}>
                     Delete
                 </button>
             </div>
